27323c84a74d25fa1d897a58852e138a9a38c962
Irving Lucas <irving.lucas@gmail.com>
2022-05-06 19:17:23 -0300

    Restruturando os layouts, definindo strings e dimensões e adicionando alguns arquivos

--- app/src/main/java/br/dev/irvinglucas/cryptomonitor/MainActivity.java
+++ app/src/main/java/br/dev/irvinglucas/cryptomonitor/MainActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 }
