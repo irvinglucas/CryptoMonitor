61ecb109a1cd01fcc575e94e39ff46734182e7f9
Irving Lucas <irving.lucas@gmail.com>
2022-05-07 23:24:26 -0300

    iniciando a programação da aplicação e retocando alguns pequenos detalhes no layout da aplicação

--- app/src/main/java/br/dev/irvinglucas/cryptomonitor/MainActivity.java
+++ app/src/main/java/br/dev/irvinglucas/cryptomonitor/MainActivity.java
@@ -2,13 +2,17 @@ package br.dev.irvinglucas.cryptomonitor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.Random;
+
+
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,4 +24,24 @@ public class MainActivity extends Activity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
     }
+
+    public void reloadButtonClick(View view) {
+
+        TextView coinValue1 = (TextView) findViewById(R.id.coin_value_1);
+        TextView coinValue2 = (TextView) findViewById(R.id.coin_value_2);
+        TextView coinValue3 = (TextView) findViewById(R.id.coin_value_3);
+        TextView coinValue4 = (TextView) findViewById(R.id.coin_value_4);
+
+        String aux;
+        Random random = new Random();
+
+        aux = String.valueOf(random.nextInt());
+        coinValue1.setText(aux);
+        coinValue2.setText(aux);
+        coinValue3.setText(aux);
+        coinValue4.setText(aux);
+    }
+
+
 }
+
